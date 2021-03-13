8663881620e9e7f5c8392a9f82502f9c6d96ce98
guangjh <guangjinhai@163.com>
2021-03-13 22:23:08 +0800

    es rest api

--- es-helloworld/src/main/java/com/gjh/learn/es/hello/utils/ESUtils.java
+++ es-helloworld/src/main/java/com/gjh/learn/es/hello/utils/ESUtils.java
@@ -1,11 +1,100 @@
 package com.gjh.learn.es.hello.utils;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * created on 2021/3/13
  *
  * @author kevinlights
  */
+@Component
 public class ESUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(ESUtils.class);
 
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.post}")
+    private int post;
+
+    @Value("${elasticsearch.scheme}")
+    private String scheme;
+
+    public RestClient getClient() {
+        return RestClient.builder(new HttpHost(host, post, scheme)).build();
+    }
+
+    public void createIndex(String idxName) {
+        LOG.info("createIndex -> {}", idxName);
+        Request request = getRequest("PUT", String.format("/%s", idxName), null, null);
+        try {
+            Response response = getClient().performRequest(request);
+            String respBody = EntityUtils.toString(response.getEntity());
+            LOG.info("createIndex -> success: \n{}", respBody);
+        } catch (IOException e) {
+            LOG.error("createIndex -> error: ", e);
+        }
+    }
+
+    public void getIndices() {
+        LOG.info("getIndices");
+        Request request = getRequest("GET", "/_cat/indices", null, null);
+        try {
+            Response response = getClient().performRequest(request);
+            String respBody = EntityUtils.toString(response.getEntity());
+            LOG.info("getIndices -> success: \n{}", respBody);
+        } catch (IOException e) {
+            LOG.error("getIndices -> error: ", e);
+        }
+    }
+
+    public void createDoc(String idxName, String docName, String docId, String docJson) {
+        LOG.info("createDoc -> {}, {}, {}, {}", idxName, docName, docId, docJson);
+        Request request = getRequest("PUT", String.format("/%s/%s/%s", idxName, docName, docId), new NStringEntity(docJson, ContentType.APPLICATION_JSON), null);
+        try {
+            Response response = getClient().performRequest(request);
+            String respBody = EntityUtils.toString(response.getEntity());
+            LOG.info("createDoc -> success: \n{}", respBody);
+        } catch (IOException e) {
+            LOG.error("createDoc -> error: ", e);
+        }
+    }
+
+    public void searchDoc(String idxName, String queryJson) {
+        LOG.info("searchDoc -> {}, {}", idxName, queryJson);
+        Request request = getRequest("GET", String.format("/%s/_search", idxName), new NStringEntity(queryJson, ContentType.APPLICATION_JSON), null);
+        try {
+            Response response = getClient().performRequest(request);
+            String respBody = EntityUtils.toString(response.getEntity());
+            LOG.info("searchDoc -> success: \n{}", respBody);
+        } catch (IOException e) {
+            LOG.error("searchDoc -> error: ", e);
+        }
+    }
+
+    public Request getRequest(String method, String endpoint, HttpEntity entity, Map<String, String> params) {
+        Request request = new Request(method, endpoint);
+        if (null != entity) {
+            request.setEntity(entity);
+        }
+        if (null != params) {
+            request.addParameters(params);
+        }
+        return request;
+    }
 
 }
--- es-helloworld/src/test/java/com/gjh/learn/es/hello/utils/ESUtilsTest.java
+++ es-helloworld/src/test/java/com/gjh/learn/es/hello/utils/ESUtilsTest.java
@@ -2,12 +2,26 @@ package com.gjh.learn.es.hello.utils;
 
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.RequestLine;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.*;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,10 +30,36 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author kevinlights
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 class ESUtilsTest {
     private static final String HOST = "127.0.0.1";
     private static final int PORT = 9200;
     private static final String SCHEME = "http";
+
+    @Autowired
+    ESUtils esUtils;
+
+    @Test
+    void createIndex() {
+        esUtils.createIndex("test");
+    }
+
+    @Test
+    void getIndices() {
+        esUtils.getIndices();
+    }
+
+    @Test
+    void createDoc() {
+        esUtils.createDoc("test", "testDoc", "1", "{\"name\": \"testName\"}");
+    }
+
+    @Test
+    void searchDoc() {
+        esUtils.searchDoc("test", "{\"query\": {\"match_all\": {}}}");
+    }
+
 
     @Test
     void testClient() throws IOException {
@@ -34,10 +74,10 @@ class ESUtilsTest {
     @Test
     void testBuilder() {
         RestClientBuilder builder = RestClient.builder(new HttpHost(HOST, PORT, SCHEME));
-        Header[] defaultHeaders = new Header[] {new BasicHeader("header", "value")};
+        Header[] defaultHeaders = new Header[]{new BasicHeader("header", "value")};
         builder.setDefaultHeaders(defaultHeaders);
 
-        builder.setFailureListener(new RestClient.FailureListener(){
+        builder.setFailureListener(new RestClient.FailureListener() {
             @Override
             public void onFailure(Node node) {
                 super.onFailure(node);
@@ -60,5 +100,78 @@ class ESUtilsTest {
         System.out.println(response.toString());
         HttpHost host = response.getHost();
         System.out.println(host.getHostName());
+
+        Cancellable cancellable = client.performRequestAsync(request, new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                System.out.println(response.toString());
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                System.out.println(exception.toString());
+            }
+        });
+        // cancellable.cancel();
+
+        request.addParameter("pretty", "true");
+        request.setEntity(new NStringEntity("{\"json\":\"text\"}", ContentType.APPLICATION_JSON));
+        request.setJsonEntity("{\"json\":\"text\"}");
+
+        RequestOptions.Builder requestBuilder = RequestOptions.DEFAULT.toBuilder();
+        requestBuilder.addHeader("Authorization", "Bearer token");
+        requestBuilder.setHttpAsyncResponseConsumerFactory(new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        RequestOptions options = requestBuilder.build();
+
+        request.setOptions(options);
+    }
+
+    @Test
+    void testAsync() throws InterruptedException {
+        RestClientBuilder builder = RestClient.builder(new HttpHost(HOST, PORT, SCHEME));
+        RestClient client = builder.build();
+
+        final CountDownLatch latch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            Request request = new Request("PUT", "/ports/doc/" + i);
+            request.setEntity(new NStringEntity("{\"json\":\"text\"}", ContentType.APPLICATION_JSON));
+            client.performRequestAsync(request, new ResponseListener() {
+                @Override
+                public void onSuccess(Response response) {
+                    latch.countDown();
+                    System.out.println(latch.getCount());
+                    RequestLine requestLine = response.getRequestLine();
+                    HttpHost host = response.getHost();
+                    response.getStatusLine().getStatusCode();
+                    Header[] headers = response.getHeaders();
+                    try {
+                        String responseBody = EntityUtils.toString(response.getEntity());
+                        System.out.println(responseBody);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Exception exception) {
+                    latch.countDown();
+                    System.out.println(latch.getCount());
+                }
+            });
+        }
+        latch.await();
+    }
+
+    @Test
+    void testAuthentication() throws IOException {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("user", "password"));
+        RestClientBuilder builder = RestClient.builder(new HttpHost(HOST, PORT, SCHEME))
+                .setHttpClientConfigCallback(httpClientBuilder -> {
+                    httpClientBuilder.disableAuthCaching();
+                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                });
+        RestClient client = builder.build();
+        client.close();
     }
 }
