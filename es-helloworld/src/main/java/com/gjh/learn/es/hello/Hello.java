cd5156b967ef8b4d8801d5e21614ab554f7e9753
guangjh <guangjinhai@163.com>
2021-03-13 10:48:13 +0800

    init commit

---
+++ es-helloworld/src/main/java/com/gjh/learn/es/hello/Hello.java
@@ -0,0 +1,16 @@
+package com.gjh.learn.es.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * created on 2021/3/13
+ *
+ * @author kevinlights
+ */
+@SpringBootApplication
+public class Hello {
+    public static void main(String[] args) {
+        SpringApplication.run(Hello.class, args);
+    }
+}
